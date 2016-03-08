cd15d59ad27bf681d8c53254092fad868d348649
LanlanXu <627363383@qq.com>
2016-03-08 13:46:51 +0800

    simple

--- javascript/simple.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	var obj=$('.wrap').eq(0),
-		i=0,j=1;
-	var timer=setInterval(function(){
-		((i+1) <= j) ? i++ : (function(){obj.append('<div class="clear"></div>'); j++; i=1; })();
-		j > 9 ? (function(){ clearInterval(timer);})() : (function(){obj.append('<span>' + i + '*' + j + '=' + i*j + '</span>');})();
-	},100);
-});
